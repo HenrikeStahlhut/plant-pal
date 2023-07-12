c8cc0670545f593eeda5faf068f83b64001728bf
Henrike Stahlhut <henrike.st@icloud.com>
2023-07-12 23:07:22 +0200

    fix: menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -7,33 +7,11 @@ import Link from "next/link";
 export default function Menu() {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   const closeMenu = () => setMenuOpen(false);
 
   // TODO: add Profile to menu
 
   return (
-    // <>
-    //   <MenuButton open={menuOpen} setOpen={setMenuOpen} />
-
-    //   <StyledMenu open={menuOpen}>
-    //     <StyledNavLink href={ROUTES.HOME} onClick={() => closeMenu()}>
-    //       Home
-    //     </StyledNavLink>
-    //     <StyledNavLink
-    //       href={ROUTES.PLANTS_OVERVIEW}
-    //       onClick={() => closeMenu()}
-    //     >
-    //       Plants
-    //     </StyledNavLink>
-    //     <StyledNavLink href={ROUTES.ROOMS_OVERVIEW} onClick={() => closeMenu()}>
-    //       Rooms
-    //     </StyledNavLink>
-    //   </StyledMenu>
-    // </>
     <>
       <MenuButton open={menuOpen} setOpen={setMenuOpen} />
       <StyledNav className="topnav" open={menuOpen}>
