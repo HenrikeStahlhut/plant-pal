cbfde8ede86b85e1db1ec0fd539bd4f98503d8ea
Henrike Stahlhut <henrike.st@icloud.com>
2023-07-12 22:59:55 +0200

    chore: home ppage edits

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout/Layout";
 import { GlobalStyle } from "@/styles/globals";
 import type { AppProps } from "next/app";
 
@@ -5,7 +6,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,21 @@
-import Link from "next/link";
-import Menu from "@/components/Menu/Menu";
-import Header from "@/components/Header/Header";
 import Greeting from "@/components/Greeting/Greeting";
 import Subheadline from "@/components/Subheadline/Subheadline";
 import TodoContainer from "@/components/TodoContainer/TodoContainer";
 import { WrapperLink } from "@/components/WrapperLink/WrapperLink.styled";
 import { ROUTES } from "@/routes";
-import Layout from "@/components/Layout/Layout";
 
 export default function Home() {
   return (
     <>
-      <Header>
-        <Greeting />
-        <Menu />
-      </Header>
-
-      <Layout>
-        <WrapperLink href={ROUTES.TODO_TODAY}>
-          <Subheadline>Todays Todo</Subheadline>
-        </WrapperLink>
-        <TodoContainer today={true} />
-        <WrapperLink href={ROUTES.TODO_UPCOMING}>
-          <Subheadline>Upcoming Todo</Subheadline>
-        </WrapperLink>
-        <TodoContainer today={false} />
-      </Layout>
+      <Greeting />
+      <WrapperLink href={ROUTES.TODO_TODAY}>
+        <Subheadline>Todays Todo</Subheadline>
+      </WrapperLink>
+      <TodoContainer today={true} />
+      <WrapperLink href={ROUTES.TODO_UPCOMING}>
+        <Subheadline>Upcoming Todo</Subheadline>
+      </WrapperLink>
+      <TodoContainer today={false} />
     </>
   );
 }
